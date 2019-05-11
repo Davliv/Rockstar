1888e2f0fd3796a139fef46292b1a3db16e6cf2b
David Israyelyan <dav93isr@gmail.com>
2019-05-11 14:23:51 +0400

    some changes for easy reading code

--- components/items/api.js
+++ components/items/api.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const ItemsAuth = express.Router();
 const bs = require('body-parser');
-const mongoose = require('mongoose');
 const validation = require('../core/validator');
 const Settings = require('../settings/status');
-const Path =  require('path');
-const multer  = require('multer');
-const Appconstants = require('../settings/constants');
 const ItemSchema = require('../items/private/model');
 const programing_db = require('../core/db');
 const items = programing_db.model('items', ItemSchema);
-
-
 const app = express();
+
 app.use(bs.json());
 app.use(bs.urlencoded({extended: false}));
 
+
+module.exports = ItemsAuth;
+
+
+
 ItemsAuth.post('/', (req, res) => {
-    const {title,type,estimation,linked_items,url,project_id,linked_optional_items,quiz_answers} = req.body;
+    const {title,type,estimation,linked_items,url,project_id,linked_optional_items,quiz_answers,content} = req.body;
     if (!title && !type && !estimation && !linked_items) {
         return res.send({success:false,msg:'Required Parameters is missing'}).status(Settings.HTTPStatus.PARAMS_INVALID);
     }
@@ -40,23 +40,24 @@ ItemsAuth.post('/', (req, res) => {
         return res.send({success:false,msg:'Not valid Answer'}).status(Settings.HTTPStatus.PARAMS_INVALID);
     }
     items.create({
-        title: title,
-        type: type,
-        estimation: estimation,
-        linked_items: linked_items,
-        url: url,
-        project_id: project_id,
-        linked_optional_items: linked_optional_items,
-        quiz_answers: quiz_answers
-      }, (err, result) => {
-      if (err) return res.send({success:false,msg:'Server error'}).status(Settings.HTTPStatus.INTERNAL_SERVER_ERROR);
-      else res.send({success:true,msg:'item created'}).status(Settings.HTTPStatus.OK);
+        title,
+        type,
+        estimation,
+        linked_items,
+        url,
+        content,
+        project_id,
+        linked_optional_items,
+        quiz_answers
+    }, (err) => {
+        if (err) return res.send({success:false,msg:'Server error'}).status(Settings.HTTPStatus.INTERNAL_SERVER_ERROR);
+        else res.send({success:true,msg:'item created'}).status(Settings.HTTPStatus.OK);
     });
 });
 
 ItemsAuth.put('/:id/url',async (req, res) => {
-  const {id} = req.params;
-  const {url}= req.body;
+  const { id } = req.params;
+  const { url }= req.body;
     if (!url && !id) {
        return res.send({success:false,msg:'missing reqiure parameter'}).status(Settings.HTTPStatus.PARAMS_INVALID);
     }
@@ -72,53 +73,34 @@ ItemsAuth.put('/:id/url',async (req, res) => {
     });
 });
 
-ItemsAuth.post('/content', (req, res) => {
-  const {content}= req.body;
-    if (content) {
-        if (!validation.validateContent(content)) {
-            return res.send({success:false,msg:'Not valid content'}).status(Settings.HTTPStatus.PARAMS_INVALID);
-        }
-        items.create({
-          content: content
-          }, (err, result) => {
-          if (err) return res.send({success:false,msg:'Server error'}).status(Settings.HTTPStatus.INTERNAL_SERVER_ERROR);
-          else res.send({success:true,msg:'content added'}).status(Settings.HTTPStatus.OK);
-        });
-    }
-});
 
-ItemsAuth.post('/:id/image', async (req, res) => {
-          const itemId=req.params.id;
-          const {imagee}=req.body;
-          if (!itemId && !image) return res.send({success:false,msg:'missing require parameter'}).status(Settings.HTTPStatus.PARAMS_INVALID);
-          let item = await items.findById(itemId);
-          if (!item)return res.send({success:false,msg:'missing require parameter'}).status(Settings.HTTPStatus.NOT_FOUND);
-          item.image.push(imagee);
-          item.save(
-            (err)=> {
-              if (err) return res.send({success:false,msg:'Server error'}).status(Settings.HTTPStatus.INTERNAL_SERVER_ERROR);
-              else  res.send({success:true,msg:'image added'}).status(Settings.HTTPStatus.OK);
+ItemsAuth.put('/:id/image', async (req, res) => {
+    const itemId=req.params.id;
+    const {imagee}=req.body;
+    if (!itemId && !image) return res.send({success:false,msg:'missing require parameter'}).status(Settings.HTTPStatus.PARAMS_INVALID);
+    let item = await items.findById(itemId);
+    if (!item)return res.send({success:false,msg:'missing require parameter'}).status(Settings.HTTPStatus.NOT_FOUND);
+    item.image.push(imagee);
+    item.save( (err)=> {
+        if (err) return res.send({success:false,msg:'Server error'}).status(Settings.HTTPStatus.INTERNAL_SERVER_ERROR);
+        else  res.send({success:true,msg:'image added'}).status(Settings.HTTPStatus.OK);
     });
 });
 
-ItemsAuth.post('/:id/answers', async (req, res) => {
-        const {value} = req.body;
-        const {correct} = req.body;
-        const {id} = req.params;
-        if (!value || !correct || !id) return res.send({success:false,msg:'missing require parameter'}).status(Settings.HTTPStatus.NOT_FOUND);
-        if (!validation.validateAnswer(value,correct)) {
-            return res.send({success:false,msg:'Not valid Answer'}).status(Settings.HTTPStatus.PARAMS_INVALID);
-        }
-        const item = await items.findById(id);
-        const itemType = item.type;
+ItemsAuth.put('/:id/answers', async (req, res) => {
+    const { value,correct } = req.body;
+    const { id } = req.params;
+    if (!value || !correct || !id) return res.send({success:false,msg:'missing require parameter'}).status(Settings.HTTPStatus.NOT_FOUND);
+    if (!validation.validateAnswer(value,correct)) {
+        return res.send({success:false,msg:'Not valid Answer'}).status(Settings.HTTPStatus.PARAMS_INVALID);
+    }
+    const item = await items.findById(id);
+    const itemType = item.type;
+    if (itemType !='quiz') return res.send({success:false,msg:'You cant add answer'}).status(Settings.HTTPStatus.PARAMS_INVALID);
+    (item.quiz_answers).push({value,correct});
+    item.save((err)=> {
+        if (err) return res.send({success:false,msg:'Server error'}).status(Settings.HTTPStatus.INTERNAL_SERVER_ERROR);
+        else  res.send({success:true,msg:'answer added'}).status(Settings.HTTPStatus.OK);
+    });
+});
 
-        if (itemType !='quiz') return res.send({success:false,msg:'You cant add answer'}).status(Settings.HTTPStatus.PARAMS_INVALID);
-        (item.quiz_answers).push({value,correct});
-        item.save(
-          (err)=> {
-          if (err) return res.send({success:false,msg:'Server error'}).status(Settings.HTTPStatus.INTERNAL_SERVER_ERROR);
-          else  res.send({success:true,msg:'answer added'}).status(Settings.HTTPStatus.OK);
-        });
-      });
-
-module.exports = ItemsAuth;
